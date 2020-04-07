aee482c7879afe0d47e8d8310db2d0a12c5f66cc
jarvis-666 <vm897@snu.edu.in>
2020-04-07 19:33:41 +0530

    Learning to use the decimal module.

--- decimal_module.py
+++ decimal_module.py
@@ -15,3 +15,33 @@ print(Decimal(0.1 + 0.2))   # undesired output
 print(Decimal(0.1 + 0.2) == Decimal(0.1) + Decimal(0.2))        # False
 
 print(float(Decimal(0.1) + Decimal(0.2)) == 0.3)    # True
+
+print(getcontext())    # outputs the current context
+# the context for arithmetic is an environment specifying precision, rounding rules, etc.
+
+# Decimal instances can be constructed from integers, strings, floats, or tuples
+
+print(Decimal(3.14))
+
+# significance of a new Decimal is determined by that of input
+# prec only affects when doing an arithmetic operation
+
+# there are two ready to use contexts: BasicContext and ExtendedContext
+
+print(BasicContext)
+print(ExtendedContext)
+
+setcontext(ExtendedContext)
+print(Decimal(1) / Decimal(0))
+
+setcontext(BasicContext)
+# print(Decimal(1) / Decimal(0))    # throw a trap
+
+# flags are set by default in BasicContext, clear them first to see the changes with every computation
+
+setcontext(ExtendedContext)
+getcontext().clear_flags()
+Decimal(22) / Decimal(7)
+print(getcontext())
+
+# Decimal objects are immutable
